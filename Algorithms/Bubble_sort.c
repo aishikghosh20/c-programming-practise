0372af8d2c7510e573e89f78e35358bf3852a745
Aishik ghosh <aishikghosh14@gmail.com>
2026-06-01 22:38:45 +0530

    Uploaded Bubble_sort.c

---
+++ Algorithms/Bubble_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+
+    int n;
+    printf("ENTER THE SIZE OF THE ARRAY\n");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i=0; i<n;i++)
+    {
+        a[i] = rand()%11; // to assign random values
+    }
+
+    // to print the array randomly generated
+    for(int i=0; i<n;i++)
+    {
+        printf("%d ", a[i]); 
+    }
+    printf("\n");
+
+    printf("\nTHE SORTED ARRAY IS :\n");
+
+    // Now to bubble sort
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j=0; j<n-1-i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+
+    for(int i=0; i<n;i++)
+    {
+        printf("%d ", a[i]); 
+    }
+
+    return 0;
+}
