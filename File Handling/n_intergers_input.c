895f6dc9c048b29b068bea9166765d720b44c665
Aishik Ghosh <aishikghosh14@gmail.com>
2026-06-09 20:32:32 +0530

    Uploaded n_integers_input.c

---
+++ File Handling/n_intergers_input.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("ENTER THE NUMBER OF INTERGERS:\n");
+    int n;
+    scanf("%d", &n);
+
+    int* a = (int*)calloc(n,sizeof(int));
+//or int* a = (int*)malloc(n * 4);
+
+    int* p = a; // to store a copy of the pointer 
+
+
+    // to input the interger numbers
+    for(int i=1; i<=n; i++)
+    {
+        printf("ENTER THE NUMBER %d", i);
+        scanf("%d", &(*a));
+        a++;
+    }
+
+    // To print using the pointer p
+      for(int i=1; i<=n; i++)
+    {
+        printf("%d ", *p);
+       
+        p++;
+    }
+
+    return 0;
+}
